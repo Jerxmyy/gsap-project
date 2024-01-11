138121f918cb492b86c5cdf23bb7eea27fed524b
Jeremy Chambon <jeremy.chambon2906@gmail.com>
2024-01-11 15:32:46 +0100

    final

--- js/script.js
+++ js/script.js
@@ -107,8 +107,9 @@ gsap.to("#neptune", {
     ease: "none",
 });
 
-
+// ajout timeline pour un scroll trigger + un zoom et fondu pour le texte 
 const timeline = gsap.timeline({
+    // scroll
     scrollTrigger: {
         trigger: "#meteors",
         start: "top 10%",
@@ -116,10 +117,14 @@ const timeline = gsap.timeline({
         pin: true
     }
 });
+
+//zoom
 timeline.to(".message-fin h1", {
     scale: 2,
     duration: 4,
 });
+
+//fondu 
 timeline.to(".message-fin h1", {
     opacity: 0,
     duration: 4,
