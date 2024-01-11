7ea121c0a60229ba3d9b0b27ed670882792361d2
Jeremy Chambon <jeremy.chambon2906@gmail.com>
2024-01-11 14:30:20 +0100

    ajout animation + CSS et SVG via HTML, compression des images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,102 @@
-gsap.to("#mercure",) 
+// animation Mercure
+gsap.to("#mercury", {
+    duration: 2,
+    motionPath: {
+        path: "#motionPath-mercury path",
+        align: "#motionPath-mercury path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+        rotate: -90,
+        repeat: -1,
+    },
+    ease: "none",
+});
+
+
+// animation Venus
+gsap.to("#venus", {
+    duration: 3,
+    motionPath: {
+        path: "#motionPath-venus path",
+        align: "#motionPath-venus path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+        rotate: -90,
+    },
+    ease: "none",
+});
+
+// animation Terre
+gsap.to("#earth", {
+    duration: 4,
+    motionPath: {
+        path: "#motionPath-earth path",
+        align: "#motionPath-earth path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+        rotate: -90,
+    },
+    ease: "none",
+});
+
+// animation Mars
+gsap.to("#mars", {
+    duration: 5,
+    motionPath: {
+        path: "#motionPath-mars path",
+        align: "#motionPath-mars path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+        rotate: -90,
+    },
+    ease: "none",
+});
+
+// animation Jupiter
+gsap.to("#jupiter", {
+    duration: 7,
+    motionPath: {
+        path: "#motionPath-jupiter path",
+        align: "#motionPath-jupiter path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+    },
+    ease: "none",
+});
+
+// animation Saturne
+gsap.to("#saturn", {
+    duration: 8,
+    motionPath: {
+        path: "#motionPath-saturn path",
+        align: "#motionPath-saturn path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+    },
+    ease: "none",
+});
+
+// animation Uranus
+gsap.to("#uranus", {
+    duration: 10,
+    motionPath: {
+        path: "#motionPath-uranus path",
+        align: "#motionPath-uranus path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+    },
+    ease: "none",
+});
+
+// animation Neptune
+gsap.to("#neptune", {
+    duration: 12,
+    motionPath: {
+        path: "#motionPath-neptune path",
+        align: "#motionPath-neptune path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+    },
+    ease: "none",
+});
+
